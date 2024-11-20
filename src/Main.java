563000d4fe7df597be1b8345bfc89d5bd93a9704
Daniel <danieldoleski02@gmail.comm>
2024-11-20 15:34:10 -0300

    Implementando a API

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
     }
 }
