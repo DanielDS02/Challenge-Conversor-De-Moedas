fc10b96abb28f0f8ada1ef5fe624986e57d25464
Daniel <danieldoleski02@gmail.comm>
2024-11-20 16:09:54 -0300

    Implementando a API

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,14 +10,18 @@ import java.net.http.HttpResponse;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/06688cb1789d692675bb121c/latest/USD"))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(System.out::println)
                 .join();
-    }
+       }
+
 }
